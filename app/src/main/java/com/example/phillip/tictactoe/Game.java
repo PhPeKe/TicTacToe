4f3152857d1e787b5001ae7c439f3b6aa11bcf93
Phillip <p.kersten@live.de>
2018-04-18 15:57:07 +0200

    Added code to the Game class and proceeded until before question 4

---
+++ app/src/main/java/com/example/phillip/tictactoe/Game.java
@@ -0,0 +1,20 @@
+package com.example.phillip.tictactoe;
+
+public class Game {
+    final private int BOARD_SIZE = 3;
+    private Tile[][] board;
+
+    private Boolean playerOneTurn;  // true if player 1's turn, false if player 2's turn
+    private int movesPlayed;
+    private Boolean gameOver;
+
+    public Game() {
+        board = new Tile[BOARD_SIZE][BOARD_SIZE];
+        for(int i=0; i<BOARD_SIZE; i++)
+            for(int j=0; j<BOARD_SIZE; j++)
+                board[i][j] = Tile.BLANK;
+
+        playerOneTurn = true;
+        gameOver = false;
+    }
+}
