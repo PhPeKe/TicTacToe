c48c7fa1a38cbe3e2e606c7e6c2621ada6ed41b9
PhPeKe <p.kerstenlive.de>
2018-04-19 02:05:07 +0200

    minor changes

--- app/src/main/java/com/example/phillip/tictactoe/Game.java
+++ app/src/main/java/com/example/phillip/tictactoe/Game.java
@@ -1,5 +1,9 @@
 package com.example.phillip.tictactoe;
 
+import static com.example.phillip.tictactoe.Tile.BLANK;
+import static com.example.phillip.tictactoe.Tile.CIRCLE;
+import static com.example.phillip.tictactoe.Tile.CROSS;
+
 public class Game {
     final private int BOARD_SIZE = 3;
     private Tile[][] board;
@@ -12,9 +16,24 @@ public class Game {
         board = new Tile[BOARD_SIZE][BOARD_SIZE];
         for(int i=0; i<BOARD_SIZE; i++)
             for(int j=0; j<BOARD_SIZE; j++)
-                board[i][j] = Tile.BLANK;
+                board[i][j] = BLANK;
 
         playerOneTurn = true;
         gameOver = false;
+    }
+    public Tile draw(int row, int column) {
+        if (board[row][column] == BLANK) {
+            if (playerOneTurn) {
+                playerOneTurn = !playerOneTurn;
+                board[row][column] = CROSS;
+                return Tile.CROSS;
+            }else {
+                playerOneTurn = !playerOneTurn;
+                board[row][column] = CIRCLE;
+                return Tile.CIRCLE;
+            }
+        }else {
+            return Tile.INVALID;
+        }
     }
 }
--- app/src/main/java/com/example/phillip/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/phillip/tictactoe/MainActivity.java
@@ -2,12 +2,20 @@ package com.example.phillip.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        game = new Game();
+    }
+    public void tileClicked (View view) {
+
     }
 }
