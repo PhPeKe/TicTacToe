d246a3b343c6e94d92e8cb3f492555c357439131
PhPeKe <p.kerstenlive.de>
2018-04-20 04:12:04 +0200

    Everything works at a basic level

--- app/src/main/java/com/example/phillip/tictactoe/Game.java
+++ app/src/main/java/com/example/phillip/tictactoe/Game.java
@@ -1,15 +1,21 @@
 package com.example.phillip.tictactoe;
 
+
+import android.support.annotation.BoolRes;
+
+import java.io.Serializable;
+
 import static com.example.phillip.tictactoe.Tile.BLANK;
 import static com.example.phillip.tictactoe.Tile.CIRCLE;
 import static com.example.phillip.tictactoe.Tile.CROSS;
 
-public class Game {
+public class Game implements Serializable{
     final private int BOARD_SIZE = 3;
     private Tile[][] board;
+    private int score;
 
-    private Boolean playerOneTurn;  // true if player 1's turn, false if player 2's turn
-    private int movesPlayed;
+    public Boolean playerOneTurn;  // true if player 1's turn, false if player 2's turn
+    public int movesPlayed = 0;
     private Boolean gameOver;
 
     public Game() {
@@ -25,15 +31,63 @@ public class Game {
         if (board[row][column] == BLANK) {
             if (playerOneTurn) {
                 playerOneTurn = !playerOneTurn;
+                movesPlayed += 1;
                 board[row][column] = CROSS;
                 return Tile.CROSS;
             }else {
                 playerOneTurn = !playerOneTurn;
+                movesPlayed += 1;
                 board[row][column] = CIRCLE;
                 return Tile.CIRCLE;
             }
         }else {
             return Tile.INVALID;
         }
+    }
+    // Check board for a winning tile-combination
+    public GameState getWin(int row, int col) {
+        score = 0;
+        for (int i = 0; i<BOARD_SIZE; i++) {
+            if (board[row][i] == CROSS) {
+                score += 1;
+                if (score == 3)
+                    return GameState.PLAYER_ONE;
+            }
+        }
+        score = 0;
+        for (int i = 0; i<BOARD_SIZE; i++) {
+            if (board[i][col] == CROSS) {
+                score += 1;
+                if (score == 3)
+                    return GameState.PLAYER_ONE;
+            }
+        }
+        score = 0;
+        for (int i = 0; i<BOARD_SIZE; i++) {
+            if (board[row][i] == CIRCLE) {
+                score += 1;
+                if (score == 3)
+                    return GameState.PLAYER_TWO;
+            }
+        }
+        score = 0;
+        for (int i = 0; i<BOARD_SIZE; i++) {
+            if(board[i][col] == CIRCLE) {
+                score += 1;
+                if (score == 3)
+                    return GameState.PLAYER_TWO;
+            }
+        }
+        score = 0;
+        if((board[0][0] == CROSS && board[1][1] == CROSS && board[2][2] == CROSS) || (board[2][0] == CROSS && board[1][1] == CROSS && board[0][2] == CROSS))
+            return GameState.PLAYER_ONE;
+
+        if((board[0][0] == CIRCLE && board[1][1] == CIRCLE && board[2][2] == CIRCLE) || (board[2][0] == CIRCLE && board[1][1] == CIRCLE && board[0][2] == CIRCLE))
+            return GameState.PLAYER_TWO;
+
+        if(movesPlayed == 9)
+            return GameState.DRAW;
+
+        return GameState.IN_PROGRESS;
     }
 }
--- app/src/main/java/com/example/phillip/tictactoe/MainActivity.java
+++ app/src/main/java/com/example/phillip/tictactoe/MainActivity.java
@@ -7,75 +7,101 @@ import android.widget.Button;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
 
+
+import java.io.Serializable;
+
+import static com.example.phillip.tictactoe.GameState.PLAYER_ONE;
+import static com.example.phillip.tictactoe.GameState.PLAYER_TWO;
+import static com.example.phillip.tictactoe.Tile.CROSS;
+
 public class MainActivity extends AppCompatActivity {
 
     Game game;
+    public boolean playerOneTurn = true;
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Button button4;
+    public Button button5;
+    public Button button6;
+    public Button button7;
+    public Button button8;
+    public Button button9;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        button3 = findViewById(R.id.button3);
+        button4 = findViewById(R.id.button4);
+        button5 = findViewById(R.id.button5);
+        button6 = findViewById(R.id.button6);
+        button7 = findViewById(R.id.button7);
+        button8 = findViewById(R.id.button8);
+        button9 = findViewById(R.id.button9);
         game = new Game();
     }
-    public void tileClicked (View view) {
+    public void tileClicked (final View view) {
         int id = view.getId();
         int row = 0;
         int col = 0;
         Button button = null;
 
-        System.out.println(id);
         switch(id) {
             case 2131165220:
-                button = findViewById(R.id.button);
+                button = button1;
                 row = 0;
                 col = 0;
                 break;
 
             case 2131165229:
-                button = findViewById(R.id.button9);
+                button = button9;
                 row = 0;
                 col = 1;
                 break;
 
             case 2131165228:
-                button = findViewById(R.id.button8);
+                button = button8;
                 row = 0;
                 col = 2;
                 break;
 
             case 2131165227:
-                button = findViewById(R.id.button7);
+                button = button7;
                 row = 1;
                 col = 0;
                 break;
 
             case 2131165226:
-                button = findViewById(R.id.button6);
+                button = button6;
                 row = 1;
                 col = 1;
                 break;
 
             case 2131165225:
-                button = findViewById(R.id.button5);
+                button = button5;
                 row = 1;
                 col = 2;
                 break;
 
             case 2131165224:
-                button = findViewById(R.id.button4);
+                button = button4;
                 row = 2;
                 col = 0;
                 break;
 
             case 2131165223:
-                button = findViewById(R.id.button3);
+                button = button3;
                 row = 2;
                 col = 1;
                 break;
 
             case 2131165222:
-                button = findViewById(R.id.button2);
+                button = button2;
                 row = 2;
                 col = 2;
                 break;
@@ -84,16 +110,47 @@ public class MainActivity extends AppCompatActivity {
 
         AlertDialog alertDialog = new AlertDialog.Builder(MainActivity.this).create();
         alertDialog.setTitle("Invalid move!");
-        alertDialog.setMessage("You cant click the same field twice!");
-        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+        alertDialog.setMessage("You cannot click the same field twice!");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "Got it",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
                     }
                 });
 
+        AlertDialog win1 = new AlertDialog.Builder(MainActivity.this).create();
+        win1.setTitle("Player One Won!");
+        win1.setMessage("Congratulations!");
+        win1.setButton(AlertDialog.BUTTON_NEUTRAL, "Thanks!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        resetClicked(view);
+                    }
+                });
+        AlertDialog win2 = new AlertDialog.Builder(MainActivity.this).create();
+        win2.setTitle("Player Two Won!");
+        win2.setMessage("Congratulations!");
+        win2.setButton(AlertDialog.BUTTON_NEUTRAL, "Thanks!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        resetClicked(view);
+                    }
+                });
+        AlertDialog pat = new AlertDialog.Builder(MainActivity.this).create();
+        pat.setTitle("Its a Draw!");
+        pat.setMessage("Nobody wins...");
+        pat.setButton(AlertDialog.BUTTON_NEUTRAL, "Try again!",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        resetClicked(view);
+                    }
+                });
 
         Tile tile = game.draw(row,col);
+        playerOneTurn = game.playerOneTurn;
         switch(tile) {
             case CROSS:
                 button.setText("X");
@@ -105,13 +162,26 @@ public class MainActivity extends AppCompatActivity {
                 alertDialog.show();
                 break;
         }
+        GameState g = game.getWin(row, col);
+
+        switch(g){
+
+            case PLAYER_ONE:
+                win1.show();
+                break;
+            case PLAYER_TWO:
+                win2.show();
+                break;
+            case DRAW:
+                pat.show();
+                break;
+        }
     }
     public void resetClicked(View view) {
 
-        AlertDialog alertDialog2 = new AlertDialog.Builder(MainActivity.this).create();
-        alertDialog2.setTitle("New Game");
-        alertDialog2.setMessage("Game is reset!");
-        alertDialog2.setButton(AlertDialog.BUTTON_NEUTRAL, "OK",
+        AlertDialog newgame = new AlertDialog.Builder(MainActivity.this).create();
+        newgame.setMessage("New Game");
+        newgame.setButton(AlertDialog.BUTTON_NEUTRAL, "Play!",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
@@ -120,26 +190,54 @@ public class MainActivity extends AppCompatActivity {
 
         game = new Game();
 
-        alertDialog2.show();
+        newgame.show();
 
-        Button button = findViewById(R.id.button);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button2);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button3);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button4);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button5);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button6);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button7);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button8);
-        button.setText("BUTTON");
-        button = findViewById(R.id.button9);
-        button.setText("BUTTON");
+        button1.setText("BUTTON");
+        button2.setText("BUTTON");
+        button3.setText("BUTTON");
+        button4.setText("BUTTON");
+        button5.setText("BUTTON");
+        button6.setText("BUTTON");
+        button7.setText("BUTTON");
+        button8.setText("BUTTON");
+        button9.setText("BUTTON");
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putBoolean("turn", playerOneTurn);
+        outState.putSerializable("game", game);
+        outState.putString("button", button1.getText().toString());
+        outState.putString("button2", button2.getText().toString());
+        outState.putString("button3", button3.getText().toString());
+        outState.putString("button4", button4.getText().toString());
+        outState.putString("button5", button5.getText().toString());
+        outState.putString("button6", button6.getText().toString());
+        outState.putString("button7", button7.getText().toString());
+        outState.putString("button8", button8.getText().toString());
+        outState.putString("button9", button9.getText().toString());
+
+    }
+
+    @Override
+    public void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        game.playerOneTurn = savedInstanceState.getBoolean("turn");
+
+        game = (Game) savedInstanceState.getSerializable("game");
+        button1.setText(savedInstanceState.getString("button"));
+        button2.setText(savedInstanceState.getString("button2"));
+        button3.setText(savedInstanceState.getString("button3"));
+        button4.setText(savedInstanceState.getString("button4"));
+        button5.setText(savedInstanceState.getString("button5"));
+        button6.setText(savedInstanceState.getString("button6"));
+        button7.setText(savedInstanceState.getString("button7"));
+        button8.setText(savedInstanceState.getString("button8"));
+        button9.setText(savedInstanceState.getString("button9"));
 
     }
 }
